cac501af4dff536552e4b3373d61dbc5da790331
TaufiqMP <taufiqmaulanapr@gmail.com>
2025-05-11 12:54:15 +0700

    About Us & Home Page - Progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import React, { useState } from "react";
 import Navbar from "./components/layout/Navbar";
 import About from "./pages/about";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
+    <div className="flex flex-col min-h-screen">
       <Navbar />
+
+      <main className="flex-grow">
+        <Routes>
+          <Route path="/product/all" element={<AllProducts />} />
+        </Routes>
+      </main>
+
+      <Footer />
+    </div>
     </>
   );
 }
